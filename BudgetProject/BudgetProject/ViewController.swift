21b732c825b51a8c6d7450c76c79a24cb2cd5120
kirill <user@MBP-User.bbrouter>
2024-05-19 14:05:55 +0300

    delete storyboard

---
+++ BudgetProject/BudgetProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BudgetProject
+//
+//  Created by User on 19.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
