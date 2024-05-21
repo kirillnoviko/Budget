7eab1c505b2ca257ee3a82bdaa249c0ec2fed067
kirill <user@MBP-User.bbrouter>
2024-05-21 23:53:41 +0300

    added OnboardingScreen

---
+++ BudgetProject/BudgetProject/UILayer/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingViewController.swift
+//  BudgetProject
+//
+//  Created by User on 19.05.24.
+//
+
+import Foundation
---
+++ BudgetProject/BudgetProject/UILayer/Onboarding/OnboardingViewPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingViewPresenter.swift
+//  BudgetProject
+//
+//  Created by User on 19.05.24.
+//
+
+import Foundation
